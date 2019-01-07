b6e43ae0cfe6a66462c839ab77a47b402c768ff5
Hai Vu <hvu20@dxc.com>
2019-01-07 16:58:30 +0700

    basic swift b1

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//khai bao
+let myConstant:Int
+let myConstant1:String = "Hello world"
+let myConstant2 = 2
+
+//cong chuoi va so
+let addVariable = "Hello world " + String(myConstant2)
+
+//chen vao chuoi
+let hiAll = "Hello \(myConstant2) you"
+
+//multiple line string
+let multiplelineString = """
+this is
+a
+multiple line
+string
+"""
